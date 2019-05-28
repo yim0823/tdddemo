09041d45c77bc9ecf3198ff87ceff35cd4826d03
Taehyoung Yim (임태형) <taehyoung.yim@bespinglobal.com>
2019-05-28 15:32:49 +0900

    Initial commit

---
+++ src/main/java/com/bespinglobal/tdddemo/TdddemoApplication.java
@@ -0,0 +1,13 @@
+package com.bespinglobal.tdddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdddemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TdddemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bespinglobal/tdddemo/TdddemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bespinglobal.tdddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TdddemoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
